13ca4337e3cba4054b69994f7cad80b424abfcea
diana.georgescu1999@gmail.com <diana.georgescu1999@gmail.com>
2020-10-25 15:07:23 +0200

    Laboratorul 3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,8 +18,12 @@ namespace Georgescu_Diana_Lab2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+   
+   
     public partial class MainWindow : Window
     {
+        private DoughnutMachine myDoughnutMachine;
+        myDoughnutMachine.DoughnutComplet+=new DoughnutMachine.DoughnutCompleteDelegate(DoughnutCompleteHandler);
         public MainWindow()
         {
             InitializeComponent();
@@ -59,5 +63,54 @@ namespace Georgescu_Diana_Lab2
         {
 
         }
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            myDoughnutMachine = new DoughnutMachine();
+        }
+        private int mRaisedGlazed;
+        private int mRaisedSugar;
+        private int mFilledLemon;
+        private int mFilledChocolate;
+        private int mFilledVanilla;
+
+        private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = true;
+            sugarToolStripMenuItem.IsChecked = false;
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Glazed);
+        }
+        private void sugarToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = false;
+            sugarToolStripMenuItem.IsChecked = true;
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Sugar);
+        }
+
+        private void DoughnutCompleteHandler()
+        {
+            switch (myDoughnutMachine.Flavor)
+            {
+                case DoughnutType.Glazed:
+                    mRaisedGlazed++;
+                    txtGlazedRaised.Text = mRaisedGlazed.ToString();
+                    break;
+
+                case DoughnutType.Sugar:
+                    mRaisedSugar++;
+                    txtSugarRaised.Text = mRaisedSugar.ToString();
+                    break;
+            }
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myDoughnutMachine.Enabled = false;
+        }
+        private void exitToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
+
 }
